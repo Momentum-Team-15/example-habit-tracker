56b3ff7291d98b961912ce8ef3751d271c8d0f89
Amy Gori <amydgori@gmail.com>
2022-11-03 11:46:49 -0400

    Add models for Habit and DailyRecord

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User, Habit, DailyRecord
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Habit)
+admin.site.register(DailyRecord)
--- core/models.py
+++ core/models.py
@@ -6,3 +6,20 @@ class User(AbstractUser):
   pass
 
 
+class Habit(models.Model):
+  name=models.CharField(max_length=255)
+  metric=models.PositiveIntegerField()
+  unit_of_measure=models.CharField(max_length=255)
+  description=models.TextField(null=True, blank=True)
+  user=models.ForeignKey(User, on_delete=models.CASCADE, related_name="habits")
+
+  def __str__(self):
+      return self.name
+
+class DailyRecord(models.Model):
+  habit=models.ForeignKey(Habit, on_delete=models.CASCADE, related_name="records")
+  date=models.DateField(auto_now_add=True)
+  amount=models.PositiveIntegerField()
+
+  def __str__(self):
+      return f"Record for {self.habit.name}"
