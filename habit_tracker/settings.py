d374b21ef97bc3b6d0746e554baab9c4e0c3f22f
Amy Gori <amydgori@gmail.com>
2022-11-09 13:59:37 -0500

    Configure for deployment

--- habit_tracker/settings.py
+++ habit_tracker/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 
-env = environ.Env(DEBUG=(bool, False), RENDER=(bool, False))
+env = environ.Env(
+  DEBUG=(bool, False),
+  RENDER=(bool, False)
+)
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -160,7 +163,11 @@ LOGIN_URL = "/auth/login"
 
 # For DRF
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ]
 }
+
+# For deployment
+if env("RENDER"):
+    ALLOWED_HOSTS.append(env("RENDER_EXTERNAL_HOSTNAME"))
